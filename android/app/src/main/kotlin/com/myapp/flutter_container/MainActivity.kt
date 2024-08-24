c3f64ef0fc11a209add605a6effef35f8948da98
Noor Mohammed Anik <noormohammedanik@gmail.com>
2024-08-24 23:11:33 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/myapp/flutter_container/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.myapp.flutter_container
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Container Example"),
+      ),
+      body: Container(
+        width: 200,
+        height: 200,
+
+        padding: EdgeInsets.all(35),
+        margin: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          //shape: BoxShape.circle,
+          shape: BoxShape.rectangle,
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(20),//when we use BoxShape.circle, we can not use it
+
+        ),
+        child: Text("Hello! I am in the container widget decoration box!!"),
+
+      ),
+    );
+  }
+}
